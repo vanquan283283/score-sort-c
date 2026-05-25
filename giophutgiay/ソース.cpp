ccd4d17473e0b4c3d750c2d3276680c53df33608
Wuaan <vanquan283283@gmail.com>
2026-05-25 17:16:47 +0900

    first commit

---
+++ giophutgiay/ソース.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <stdio.h>
+	int main(void)
+	{
+		int gio, phut, giay, num;
+		printf("整数を入力してください\n", &num);
+		scanf_s("%d", &num);
+		gio = num / 3600;
+		phut = (num % 3600) / 60;
+		giay = num % 60;
+		
+		printf(" %d時%d分%d秒", gio, phut, giay);
+		return 0;
+
+
+
+	return 0;
+
+	
+}
